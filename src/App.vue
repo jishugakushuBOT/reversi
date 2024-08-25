b1206713e28ee2779f446ed69f39b2aadfa3dca6
Kamiyu Yano <a5820091@aoyama.jp>
2024-08-26 02:19:43 +0900

    ゲームの仕様完成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="container">
-    <div class="board">
-      <div class="beside" v-for="(cell,columnIndex) in cells" :key="columnIndex">
-        <div class="vertical" v-for="(disk,rowIndex) in cell" :key="disk.index" @click="placePieces(columnIndex,rowIndex)">
-          <div v-if="disk==0 && turnAble[columnIndex][rowIndex]===1" class="disk yellow"></div>
-          <div v-if="disk==0 && turnAble[columnIndex][rowIndex]!==1" class="disk none"></div>
-          <div v-if="disk==1" class="disk white"></div>
-          <div v-if="disk==-1" class="disk black"></div>
+  <div class="fullDisplay">
+    <div class="container">
+      <div class="board">
+        <div class="beside" v-for="(cell,columnIndex) in cells" :key="columnIndex">
+          <div class="vertical" v-for="(disk,rowIndex) in cell" :key="disk.index" @click="placePieces(columnIndex,rowIndex)">
+            <div v-if="disk==0 && turnAble[columnIndex][rowIndex]===1" class="disk yellow"></div>
+            <div v-if="disk==0 && turnAble[columnIndex][rowIndex]!==1" class="disk none"></div>
+            <div v-if="disk==1" class="disk white"></div>
+            <div v-if="disk==-1" class="disk black"></div>
+          </div>
         </div>
       </div>
     </div>
+    <div class="footer">
+      <p v-if="this.player===-1">黒の番です</p>
+      <p v-if="this.player===1">白の番です</p>
+      <button @click="restart()">リスタート</button>
+    </div>
   </div>
-
 </template>
 
 <script>
@@ -46,16 +52,18 @@ export default {
         [-1, -1], [-1, 1], [1, -1], [1, 1]
       ],
       // playerは先行（黒）を-1、後行（白）を1とします
-      player: -1
+      player: -1,
+      passFlag:false
     }
   },
 
+
   mounted() {
     this.checkTurnable();
-    console.log("aaaaaaaaa")
   },
 
   methods:{
+    //プレイヤーが石を置ける場所を判定
     checkTurnable() {
       this.turnAble=[
         [0,0,0,0,0,0,0,0],
@@ -67,6 +75,7 @@ export default {
         [0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0],
       ]
+      
       for (let i = 0; i < this.cells.length; i++) {
         for (let j = 0; j < this.cells[i].length; j++) {
           if (this.cells[i][j] === 0) {
@@ -88,6 +97,8 @@ export default {
         }
       }
     },
+
+    //ポインターが枠内か判定する
     isValidPosition(x, y) {
       return x >= 0 && x < 8 && y >= 0 && y < 8;
     },
@@ -102,12 +113,12 @@ export default {
           let x = columnIndex+dx
           let y = rowIndex+dy
           let hasopponent = false
-          while(this.isValidPosition && this.cells[x][y]===-this.player){
+          while(this.isValidPosition(x,y) && this.cells[x][y]===-this.player){
             x += dx
             y += dy
             hasopponent = true
           }
-          if(this.isValidPosition && hasopponent && this.cells[x][y]===this.player){
+          if(this.isValidPosition(x,y) && hasopponent && this.cells[x][y]===this.player){
             let pointerX = columnIndex + dx
             let pointerY = rowIndex + dy
             while(!(pointerX===x && pointerY===y)){
@@ -117,20 +128,61 @@ export default {
             }
           }
         }
-
+        //プレイヤーをチェンジ
         this.player=-this.player
+        // 次のプレイヤーが石を置ける場所をチェック
         this.checkTurnable()
-        console.log("faefefe")
+
+        //もし次のプレイヤーが石を置ける場所がなかったら
+        if(this.turnAble.every(row => row.every(value => value===0))){
+          console.log("置ける場所がない")
+          //パスする（プレイヤーを入れ替える）
+          this.player=-this.player
+          this.checkTurnable()
+          //入れ替わったプレイヤーも置くところがなかったら（ゲーム終了）
+          if(this.turnAble.every(row => row.every(value => value===0))){
+            console.log("ゲーム終了")
+            let Sum = this.cells.flat().reduce((sum,num) => sum+num,0)
+            if (Sum > 0) {
+              console.log("白の勝ち");
+            } else if (Sum < 0) {
+              console.log("黒の勝ち");
+            } else {
+              console.log("引き分け");
+            }
+          }
+        }
+        
+        
       }
+    },
+
+    restart(){
+      this.cells=[
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,1,-1,0,0,0],
+        [0,0,0,-1,1,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+      ]
+      this.player=-1
+      this.checkTurnable()
+      
     }
   }  
 }
 </script>
 
 <style>
+.fullDisplay{
+  width: 430px;
+}
 .container{
   background-color: #333;
-  width: 430px;
+  width: 100%;
   height: 470px;
   display:flex;
   align-items:center;
@@ -180,6 +232,12 @@ export default {
   width:20%;
   height:20%;
 }
+
+.footer{
+  width: 430px;
+  text-align: center;
+}
+
 </style>
 
 
